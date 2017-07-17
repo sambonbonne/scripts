c560f22def58c7164ce5be78197e43e35c56c37e
Samuel DENIS <samuel@denis.tech>
2017-07-17 21:58:46 +0200

    Add ability to print metadata stats

--- music-stats.sh
+++ music-stats.sh
@@ -7,6 +7,21 @@ list_music() {
   find "${MUSIC_FOLDER}" -type f | sed -e "s/^${escaped}\///"
 }
 
+metadata() {
+  find "${MUSIC_FOLDER}" -type f \
+    | xargs -n 1 ffprobe 2>&1 \
+    | grep "$(echo ${1} | tr [a-z] [A-Z])" \
+    | cut -d':' -f 2 \
+    | sed -e 's/^[[:space:]]*//' \
+    | sort | uniq -c
+}
+
 echo "    $(list_music | wc -l) *"
 
 list_music | rev | cut -d'.' -f 1 | rev | sort | uniq -c
+
+echo ""
+
+command -v ffprobe >/dev/null || { echo "If you install ffmpeg ffprobe, you'll have more stats"; exit 0; }
+
+metadata "artist"
